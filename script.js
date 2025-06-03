103068e218392c48029c79170a0541d2a35b0b7a
Maël Auzenet <mauzenet@gmail.com>
2025-06-03 15:20:42 +0200

    add pastille

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const lenis = new Lenis({
 });
 
 
+  if (window.innerWidth <= 1024) {
+    const items = document.querySelectorAll('.pastille-item');
+
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        // Retirer la classe hover des autres items
+        items.forEach(i => {
+          if (i !== item) i.classList.remove('hover');
+        });
+        // Toggle la classe hover
+        item.classList.toggle('hover');
+      });
+    });
+  }
+
+
