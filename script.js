8960dcf089e362cc5c679f9766fe576657e39fd8
Maël Auzenet <mauzenet@gmail.com>
2025-06-04 14:52:07 +0200

    burger responsif + essentiel font

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ const lenis = new Lenis({
 });
 
 
-  if (window.innerWidth <= 1024) {
-    const items = document.querySelectorAll('.pastille-item');
+if (window.innerWidth <= 1024) {
+  const items = document.querySelectorAll('.pastille-item');
 
-    items.forEach(item => {
-      item.addEventListener('click', () => {
-        // Retirer la classe hover des autres items
-        items.forEach(i => {
-          if (i !== item) i.classList.remove('hover');
-        });
-        // Toggle la classe hover
-        item.classList.toggle('hover');
+  items.forEach(item => {
+    item.addEventListener('click', () => {
+      items.forEach(i => {
+        if (i !== item) i.classList.remove('hover');
       });
+      item.classList.toggle('hover');
     });
+  });
+}
+
+
+const burger = document.querySelector('.burger-icon');
+const sideMenu = document.querySelector('.side-menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  sideMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', function(event) {
+  const burger = document.querySelector('.burger-icon');
+  const sideMenu = document.querySelector('.side-menu');
+
+  const isClickInsideBurger = burger.contains(event.target);
+  const isClickInsideMenu = sideMenu.contains(event.target);
+
+  if (!isClickInsideBurger && !isClickInsideMenu && sideMenu.classList.contains('active')) {
+    sideMenu.classList.remove('active');
+    burger.classList.remove('active');
   }
-
-
+});
